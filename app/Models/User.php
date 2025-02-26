2345eed812c57d1ef4c19f37ad2a718b6b8a1062
Bleri123 <bleonnasufi9@gmail.com>
2025-02-26 14:26:08 +0100

    added userinfo model and relationship done

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\UserInfo;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -51,5 +52,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function userInfo()
+    {
+        return $this->hasOne(UserInfo::class, 'user_id', 'id');
     }
 }
