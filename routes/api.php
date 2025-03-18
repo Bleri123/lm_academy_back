9927176cb72ea119204318e9bbef6ebe033d9c6f
Bleri123 <bleonnasufi9@gmail.com>
2025-03-18 22:45:02 +0100

    authentication and authorization

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/test-api-endpoint', function (Request $request) {
     return response()->json([
@@ -9,4 +10,15 @@ Route::get('/test-api-endpoint', function (Request $request) {
         'parameters' => $request->all(),
         'moral' => $request->query('Moral')
     ], 200);
+});
+
+Route::controller(AuthController::class)->prefix('auth')->middleware('api')->group(function(){
+    Route::post('/login', 'login')->name('auth.login');
+    Route::post('/register', 'register')->name('auth.register');
+    Route::post('/refresh', 'refresh')->name('auth.refresh');
+
+    Route::middleware('jwt.auth.token')->group(function(){
+        Route::post('/logout', 'logout')->name('auth.logout');
+        Route::get('/user-profile', 'userProfile')->name('auth.userProfile');
+    });
 });
